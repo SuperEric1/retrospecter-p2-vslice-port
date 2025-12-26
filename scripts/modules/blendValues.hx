ff9d4ab96a7698d9d6173761fad5fa348db738d4
SuperEric <75173041+SuperEric1@users.noreply.github.com>
2025-12-25 23:24:02 -0300

    phase 4 add

---
+++ scripts/modules/blendValues.hx
@@ -0,0 +1,16 @@
+// useful
+static var ADD:Int = 0;
+static var ALPHA:Int = 1;
+static var DARKEN:Int = 2;
+static var DIFFERENCE:Int = 3;
+static var ERASE:Int = 4;
+static var HARDLIGHT:Int = 5;
+static var INVERT:Int = 6;
+static var LAYER:Int = 7;
+static var LIGHTEN:Int = 8;
+static var MULTIPLY:Int = 9;
+static var NORMAL:Int = 10;
+static var OVERLAY:Int = 11;
+static var SCREEN:Int = 12;
+static var SHADER:Int = 13;
+static var SUBTRACT:Int = 14;
